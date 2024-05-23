1187b0faa133bc0a397027d1626c0cf64ea89875
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-05-23 20:15:00 +1000

    created OrderCreator

--- backend/logic/order_creator.py
+++ backend/logic/order_creator.py
@@ -1,5 +1,18 @@
-
+from order_mediator import OrderMediator
 class OrderCreator:
 
-    def __init__(self):
-        pass
+    _order_mediator: OrderMediator
+
+    def __init__(self, order_mediator) -> None:
+        self._order_mediator = order_mediator
+    
+    def confirm_payment(self, order_dict: dict) -> bool:
+        # Will typically check with a third party vendor for payment,
+        # however that has been skipped in this case.
+
+        return True
+
+    def create_order(self, order_dict) -> bool:
+        self._order_mediator.create_order(order_dict)
+        
+        return True
